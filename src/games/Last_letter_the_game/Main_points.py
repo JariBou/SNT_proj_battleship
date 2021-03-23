1f9f27322f466b6cf5290ff8cb8db1a42f3fab39
JariBou <jaribouyt@gmail.com>
2021-03-23 11:31:56 +0100

    Added Last_letter_game

--- src/games/Last_letter_the_game/Main_points.py
+++ src/games/Last_letter_the_game/Main_points.py
@@ -1,5 +1,7 @@
 from random import randint
 
+###THIS IS THE MAIN GAME###
+
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
             "v", "w", "x", "y", "z"]
 voyelles = ["a", "e", "i", "o", "u"]
