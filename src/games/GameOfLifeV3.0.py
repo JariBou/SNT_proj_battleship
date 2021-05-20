7c6c013184fd46161349f75b1fd2854f0c14f255
JariBou <jaribouyt@gmail.com>
2021-05-20 20:33:50 +0200

    Work on demineur and GameOfLiveV2.0.py

--- src/games/GameOfLifeV3.0.py
+++ src/games/GameOfLifeV3.0.py
@@ -30,6 +30,13 @@ def about():
 
 ####                                  ####
 
+def create_menu(menubar: tk.Menu):
+    menubar.add_command(label="Help", command=g_help)
+    menubar.add_command(label="About", command=about)
+    # menubar.add_command(label="Stats", command=self.stats)
+    #menubar.add_command(label="Game Select Menu", command=lambda: [self.root.destroy(), run_main.run_main()])
+
+
 class Game:
 
     def __init__(self):
@@ -41,7 +48,7 @@ class Game:
         pg.init()
         self.root = pg.Surface((self.nb_columns*self.square_dim, self.nb_lines*self.square_dim))
         self.screen = pg.display.set_mode((self.nb_columns*self.square_dim, self.nb_lines*self.square_dim))
-        pg.display.set_caption('Game Of Life V3')
+        pg.display.set_caption('Game Of Life V3 - idle')
         pg.display.flip()
 
         self.t1 = None
@@ -61,6 +68,7 @@ class Game:
 
         running = True
         self.time = 0.05
+        self.percentage = 10
 
         self.init_lvl()
 
@@ -69,17 +77,10 @@ class Game:
                 if event.type == pg.QUIT:
                     running = False
                 elif event.type == pg.KEYDOWN:
-                    # if event.key == pg.K_LEFT:
-                    #     self.pos[0] -= 5
-                    # if event.key == pg.K_RIGHT:
-                    #     self.pos[0] += 5
-                    # if event.key == pg.K_UP:
-                    #     self.pos[1] -= 5
-                    # if event.key == pg.K_DOWN:
-                    #     self.pos[1] += 5
                     if event.key == pg.K_RETURN:
                         self.playing = True if not self.playing else False
                         print('playing now enabled' if self.playing else 'playing now disabled')
+                        pg.display.set_caption(f'Game Of Life V3 - {"running" if self.playing else "idle"}')
                     if event.key == pg.K_r:
                         self.randomize()
                     if event.key == pg.K_c:
@@ -92,13 +93,23 @@ class Game:
                         self.time = 0.05
                     if event.key == pg.K_q:
                         self.time = 0.1
-                    # self.draw_rect(tuple(self.pos), self.WHITE)
+                    if event.key == pg.K_h:
+                        root = tk.Tk()
+                        root.title('Help')
+                        tk.Button(root, text='change speed', command=lambda: self.set_time(0.5)).pack()
+                        self.change_rand_menu(root)
+                        self.change_size_menu(root)
+                        tk.Button(root, text='Done', command=lambda: root.destroy()).pack()
+                        Ct.center(root)
+                        root.mainloop()
                 elif event.type == pg.MOUSEBUTTONUP:
                     self.place(pg.mouse.get_pos())
             if self.playing:
                 self.round()
-            #self.round()
             pg.display.update()
+
+    def set_time(self, x: float):
+        self.time = x
 
     def init_lvl(self):
         self.to_born = {}
@@ -130,8 +141,8 @@ class Game:
         print('randomizing')
         for x in range(1, self.nb_columns+1):
             for y in range(1, self.nb_lines+1):
-                rand = randint(0, 3)
-                if rand == 1:
+                rand = randint(0, 100)
+                if rand <= self.percentage:
                     self.alive[x, y] = 1
                     self.draw_rect(((x - 1) * self.square_dim, (y - 1) * self.square_dim), self.WHITE)
         print('done')
@@ -139,6 +150,7 @@ class Game:
     def clear_board(self):
         print('playing now disabled - clearing...')
         self.playing = False
+        pg.display.set_caption(f'Game Of Life V3 - idle')
         for x in range(1, self.nb_columns+1):
             for y in range(1, self.nb_lines+1):
                 self.alive[x, y] = 0
@@ -157,23 +169,15 @@ class Game:
             for line in range(min_line, max_line + 1):
                 if self.alive[col, line] == 1:
                     ans += 1
-        #
-        # ans = len([key for key in self.alive.keys() if ((self.alive[key] == 1)
-        #             & (min_line <= key[1] <= max_line)
-        #             & (min_column <= key[0] <= max_column))])
-        #
-        # if self.alive[col, line] == 1:
-        #     ans -= 1
         return ans
 
     def update_dicts(self):
         for key in self.test_dict.keys():
             is_born = True if (self.test_dict.get(key) == 3 and (self.alive[key] == 0)) else False
-            keeps_alive = True if ((self.test_dict.get(key) in [2, 3]) and self.alive[key] == 1) else False
             if is_born:
                 self.draw_rect(((key[0] - 1) * self.square_dim, (key[1] - 1) * self.square_dim), self.WHITE)
             dies = False if (self.test_dict.get(key) in [2, 3]) or (self.alive[key] == 0) else True
-            self.alive[key] = 1 if (is_born or keeps_alive) else 0
+            self.alive[key] = 1 if (is_born or ((self.test_dict.get(key) in [2, 3]) and self.alive[key] == 1)) else 0
             if dies:
                 self.draw_rect(((key[0] - 1) * self.square_dim, (key[1] - 1) * self.square_dim), self.GREY)
 
@@ -184,30 +188,7 @@ class Game:
         self.update_dicts()
         time.sleep(self.time)
 
-    def half_t(self):
-        for y in range(self.nb_lines//4, self.nb_lines//4*2+1):
-            for x in range(1, self.nb_columns + 1):
-                self.test_dict[x, y] = self.get_neighbours(x, y)
-
-    def half_t2(self):
-        for y in range(self.nb_lines//4*2+1, self.nb_lines//4*3+1):
-            for x in range(1, self.nb_columns + 1):
-                self.test_dict[x, y] = self.get_neighbours(x, y)
-
-    def half_t3(self):
-        for y in range(self.nb_lines//4*3+1, self.nb_lines + 1):
-            for x in range(1, self.nb_columns + 1):
-                self.test_dict[x, y] = self.get_neighbours(x, y)
-
     def update_gui(self):
-        tb_keys = self.to_born.keys()
-        tk_keys = self.to_kill.keys()
-        # for y in range(1, self.nb_lines + 1):
-        #     for x in range(1, self.nb_columns + 1):
-        #         if self.alive[x, y] == 1:
-        #             self.draw_rect(((x-1) * self.square_dim, (y-1) * self.square_dim), self.WHITE)
-        #         else:
-        #             self.draw_rect(((x-1) * self.square_dim, (y-1) * self.square_dim), self.GREY)
         for place in self.to_born.keys():
             self.draw_rect(((place[0] - 1) * self.square_dim, (place[1] - 1) * self.square_dim), self.WHITE)
         for place in self.to_kill.keys():
@@ -224,11 +205,47 @@ class Game:
         pg.draw.rect(self.square_surface, color, self.square_surface.get_rect())
         self.screen.blit(self.square_surface, position)
 
-    def create_menu(self, menubar: tk.Menu):
-        menubar.add_command(label="Help", command=g_help)
-        menubar.add_command(label="About", command=about)
-        # menubar.add_command(label="Stats", command=self.stats)
-        #menubar.add_command(label="Game Select Menu", command=lambda: [self.root.destroy(), run_main.run_main()])
+    def change_rand_menu(self, window):
+        tk.Label(window, text=f"Enter a percentage between 1 and 99 included: (curr: {self.percentage})").pack()
+        entry = tk.Entry(window)
+        entry.pack()
+        tk.Button(window, text="Confirm", command=lambda: self.change_rand_val(entry)).pack()
+
+    def change_rand_val(self, entry: tk.Entry):
+        val = entry.get()
+        entry.delete(0, len(val))
+        if not (0 < int(val) < 100):
+            entry.insert(0, 'wrong range')
+            return
+        entry.insert(0, f'Updated to: {val}')
+        self.percentage = int(val)
+
+    def change_sizes(self, nCol, nLine, square_dim):
+        self.clear_board()
+        self.nb_columns = nCol
+        self.nb_lines = nLine
+        self.square_dim = square_dim
+        self.square_size = (self.square_dim, self.square_dim)
+        self.square_surface = pg.Surface(self.square_size)
+        self.root = pg.Surface((self.nb_columns * self.square_dim, self.nb_lines * self.square_dim))
+        self.screen = pg.display.set_mode((self.nb_columns * self.square_dim, self.nb_lines * self.square_dim))
+        pg.display.set_caption('Game Of Life V3')
+        pg.display.flip()
+        self.init_lvl()
+
+    def change_size_menu(self, window):
+        tk.Label(window, text="Enter new dimensions as nColxnLine+squareSide").pack()
+        tk.Label(window, text=f"Current size: {self.nb_columns}x{self.nb_lines}+{self.square_dim}").pack()
+        entry = tk.Entry(window)
+        entry.pack()
+        tk.Button(window, text="Confirm", command=lambda: self.change_size_val(entry)).pack()
+
+    def change_size_val(self, entry: tk.Entry):
+        string = entry.get()
+        vals = string.split('+')
+        squareDim = int(vals.pop())
+        nCol, nLine = vals[0].split('x')
+        self.change_sizes(int(nCol), int(nLine), squareDim)
 
 
 if __name__ == '__main__':
