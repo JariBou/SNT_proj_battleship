baed3f936410db0170b94024338b63c3aa0d09bd
mathis <mathis.guillot1@gmail.com>
2021-05-26 15:03:36 +0200

    Added all the fucking icon/textures except gameoflife in the run_main

--- src/run_main.py
+++ src/run_main.py
@@ -13,23 +13,33 @@ class run_main:
         self.w = Tk()
         self.w.title("Choose GameMode")
         self.w.config(bg="lightgray")
+        self.path = Ct.get_path()
+        self.images  = {'morpion':PhotoImage(file='resources\\images\\TicTacToe\\tictactoe.png'),
+                        'battleship':PhotoImage(file='resources\\images\\Battleship\\battleship_icon.png'),
+                        'chess':PhotoImage(file='resources\\images\\Chess\\taskbar_img_2.png'),
+                        'shifumi':PhotoImage(file='resources\\images\\rock_paper_scissors\\shifumi_icon.gif'),
+                        'hangman':PhotoImage(file='resources\\images\\Hangman\\pendu_icon.gif'),
+                        'demineur':PhotoImage(file='resources\\images\\Demineur\\mine.png'),
+                        'snake':PhotoImage(file='resources\\images\\snake\\snake.png')}
+
+        """'GoL':Ct.get_img(self.path.joinpath('ressources\\images\\GoL\\GoL.png''))"""
 
         customFont = ft.Font(size=20)
 
         # Set all buttons for apps
-        buttonList = [Button(text="Morpion", command=lambda: (self.w.destroy(), Runmorpion.run()), font=customFont),
-                      Button(text="Battleship", command=lambda: (self.w.destroy(), Battleship.Battleship_1v1()),
+        buttonList = [Button(text="Morpion", image=self.images['morpion'], command=lambda: (self.w.destroy(), Runmorpion.run()), font=customFont),
+                      Button(text="Battleship", image=self.images['battleship'], command=lambda: (self.w.destroy(), Battleship.Battleship_1v1()),
                              font=customFont),
-                      Button(text="Echecs", command=lambda: (self.w.destroy(), run_chess.ChessGui()), font=customFont),
+                      Button(text="Echecs", image=self.images['chess'], command=lambda: (self.w.destroy(), run_chess.ChessGui()), font=customFont),
                       Button(text="Jeu de la Vie", command=lambda: (self.w.destroy(), run_gol.run()), font=customFont),
                       Button(text="Dernière Lettre", command=lambda: (self.w.destroy(), Last_letter_game.Last_letter()),
                              font=customFont),
-                      Button(text="Shifumi", command=lambda: (self.w.destroy(), Rock_paper_scissors.Game()),
+                      Button(text="Shifumi", image=self.images['shifumi'], command=lambda: (self.w.destroy(), Rock_paper_scissors.Game()),
                              font=customFont),
-                      Button(text="Hangman", command=lambda: (self.w.destroy(), Pendu_gui.Game('Lexique')),
+                      Button(text="Hangman", image=self.images['hangman'], command=lambda: (self.w.destroy(), Pendu_gui.Game('Lexique')),
                              font=customFont),
-                      Button(text="Démineur", command=lambda: (self.w.destroy(), Demineur.Game()), font=customFont),
-                      Button(text="Snake", command=lambda: (self.w.destroy(), Snake.Game()), font=customFont)]
+                      Button(text="Démineur", image=self.images['demineur'], command=lambda: (self.w.destroy(), Demineur.Game()), font=customFont),
+                      Button(text="Snake", image=self.images['snake'], command=lambda: (self.w.destroy(), Snake.Game()), font=customFont)]
 
         size = 3
         while len(buttonList) % size != 0:
